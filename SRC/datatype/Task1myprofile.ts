05e2d66d6be26fe11a0d63a0b9c7cde009f7b68d
Gowrisankarmahesh <Gowri@#>
2026-06-11 16:49:41 +0530

    fixed

--- SRC/datatype/Task1myprofile.ts
+++
@@ -1,6 +0,0 @@
-let username : string = "PV Mahesh";
-let age : number = 31;
-const Experience : number = 5;
-const place :  string = "coimbatore";
-let summary : string = `have ${Experience}years of expereince in IT Recruitment.currently i am not working`;
-console.log(username,age,Experience,location,summary);
