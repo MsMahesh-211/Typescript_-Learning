0e0922093d8ee4a7dc69b71d07a68e79ad617a50
Gowrisankarmahesh <Gowri@#>
2026-06-12 16:11:35 +0530

    add

---
+++ SRC/controlFlow/Nestedif.ts
@@ -0,0 +1,11 @@
+let username = "admin";
+let password = "1234";
+if (username === "Mahesh") {
+ if (password === "2345") {
+ console.log("Login successful ");
+ } else {
+ console.log("Wrong password ");
+ }
+} else {
+ console.log("Invalid username ");
+}
