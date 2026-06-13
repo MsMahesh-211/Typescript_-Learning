53d54d567ca12f393579aac02e73fba81b756cf0
Gowrisankarmahesh <Gowri@#>
2026-06-13 18:56:02 +0530

    add

---
+++ SRC/controlFlow/task.ts
@@ -0,0 +1,16 @@
+let mobilenumber = 12345;
+let username = "Mahesh";
+let email = "pvmahesh211@gmail.com"
+let password = 1234;
+if (mobilenumber===12345) {
+    if(username==="Mahi"|| email === "pvmahesh@gmail.com") {
+      if(password===1234)
+        console.log("login is successfull");
+    else
+        console.log("login is not successfull");
+      }
+      else
+        console.log("invalid mobile number,username or email");
+    
+    }
+    
