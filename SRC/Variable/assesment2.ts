68eaa06c99c873932eeda3d3b1f17172839014cc
Gowrisankarmahesh <Gowri@#>
2026-06-15 12:16:28 +0530

    add

---
+++ SRC/Variable/assesment2.ts
@@ -0,0 +1,14 @@
+//let mahiage = 24;
+//let mahicity = "coimbatore";
+//const country="India";
+//console.log(mahiage,mahicity,country);
+//enum colors{    Blue = "BLUE", Green = "GREEN" }
+//let Value: colors= colors.Blue;
+//console.log(colors);
+//var score = 50;
+//if (true){var score=80;}
+//console.log(score);
+
+let rank = 10;
+if (true){let rank=20;}
+console.log(rank);
