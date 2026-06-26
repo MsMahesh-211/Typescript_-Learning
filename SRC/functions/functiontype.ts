f1d385639e103b82962609fa2e6de2cde05e1af1
Gowrisankarmahesh <Gowri@#>
2026-06-26 16:22:18 +0530

    add

---
+++ SRC/functions/functiontype.ts
@@ -0,0 +1,19 @@
+// //Named Function
+// function add(a:number,b:number): number{
+//     return a+b;
+
+// }
+// console.log(add(3,4));
+// let sum1=3;
+// let sum2=4;
+// console.log(add(sum1,sum2));
+//Arrow Function
+// const cube=(x:number)=>x*x*x;
+// console.log(cube(8));
+//Anonymous Function
+const greet=function(name:string):string{
+    return `Hello ${name}`;
+}
+console.log(greet('Mahesh'));
+
+
