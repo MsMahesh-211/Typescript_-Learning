0d04dc212b8b484020011d487b5d8d13985719ba
Gowrisankarmahesh <Gowri@#>
2026-06-26 21:53:30 +0530

    add

---
+++ SRC/arrays/samplecode.ts
@@ -0,0 +1,22 @@
+// let nums :number[]=[10,20,30];
+// let mixed:(number | string)[]=['hi',10];
+// console.log(nums);
+// console.log(mixed);
+//boolean
+let nums :number[]=[10,20,30];
+let mixed:(number | string |boolean)[]=['hi',10,true];
+console.log(nums);
+console.log(mixed);
+//code 2
+// let flowers:Array<string>=['rose','jasmine','lily'];
+// flowers.push('Tulip');
+// console.log(flowers);
+//code 3 using index
+// let flowers:Array<string>=['rose','jasmine','lily'];
+// flowers[0]='Tulip';
+// flowers[1]='Rose'
+// console.log(flowers);
+//code 4 using const
+const flowers:Array<string>=['rose','jasmine','lily'];
+flowers[0]='Tulip';
+console.log(flowers);
