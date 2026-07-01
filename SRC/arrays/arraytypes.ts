8363fbfa4611de3d00eb11b5c4208c774f33fa8a
Gowrisankarmahesh <Gowri@#>
2026-07-01 13:29:02 +0530

    add

---
+++ SRC/arrays/arraytypes.ts
@@ -0,0 +1,18 @@
+//for each()-easy levels.
+const items:string[] =["applephone","samsung","nokia","oneplus"];
+items.forEach(function(item){
+    console.log("E1 Items:",items);
+});
+const nums: number[] = [10, 20, 30];
+nums.forEach(function (n, i) {
+  console.log(`E2 Index ${i} = ${n}`);
+});
+const colors: string[] = ["red", "green", "blue"];
+colors.forEach((color) => console.log("E3 Color:", color));
+//medium
+let total=0;
+
+
+    
+
+
