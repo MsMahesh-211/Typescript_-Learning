2d945afd2bf953f0383b5792c7b7daf1c8d09f94
Gowrisankarmahesh <Gowri@#>
2026-07-15 13:22:30 +0530

    add Bank and Customer classes with methods for displaying customer details

---
+++ SRC/oops/encapsulation/samplecode_Bank.ts
@@ -0,0 +1,44 @@
+//Parent Class
+class Bank{
+    public customerName:string;
+    protected accountNo:number;
+    private balance:number;
+    constructor(customerName:string,accountNo:number,balance:number){
+        this.customerName=customerName;
+        this.accountNo=accountNo;
+        this.balance=balance;
+    }
+    //getter method
+    getBalance():number{
+        return this.balance;
+    }
+    //to access protected variable from outside the class we can use getter method
+    getAccountNumber():number{
+        return this.accountNo;
+    }
+}
+class Customer extends Bank{
+    public branchName:string;
+    constructor(customerName:string,accountNo:number,balance:number,branchName:string){
+        super(customerName,accountNo,balance);
+        this.branchName=branchName;
+    }
+    displayCustomer(){
+        console.log(`Customer_Name:${this.customerName},Customer_AccountNo:${this.getAccountNumber()},Customer_Balance:${this.getBalance()},Customer_BranchName:${this.branchName}`);
+    }
+    displayBank(){
+        console.log(`Customer_Name:${this.customerName},Customer_AccountNo:${this.getAccountNumber()},Customer_Balance:${this.getBalance()}`);
+
+    }
+}
+//object creation
+const customer=new Customer("Mahesh",123456789,50000,"Perundurai");
+customer.displayBank();
+console.log("================================");
+customer.displayCustomer();
+console.log("================================");
+console.log(`Customer_Name:${customer.customerName}`);
+ //public variable can be accessed outside the class
+ console.log(`Customer_AccountNo:${customer.getAccountNumber()}`);
+ //protected variable can be accessed through getter method
+ console.log(`Customer_BranchName:${customer.branchName}`);
